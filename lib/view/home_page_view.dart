850f417da808405d0725593fb07441a98130bb25
vinihzets <viniciusdaarky@outlook.com>
2023-07-27 13:26:30 -0300

    atualizacao na view, melhorado layout e adicionado gridView com informacoes

--- lib/view/home_page_view.dart
+++ lib/view/home_page_view.dart
@@ -12,6 +12,7 @@ class HomePageView extends StatefulWidget {
 class _HomePageViewState extends State<HomePageView> {
   late TextEditingController pesoController;
   late TextEditingController alturaController;
+  double? imc;
 
   @override
   void initState() {
@@ -22,16 +23,39 @@ class _HomePageViewState extends State<HomePageView> {
   }
 
   calculate(double peso, double altura, BuildContext context) {
+    String information = '';
     if (peso > 0 && altura > 0) {
-      double imc = peso / (altura * altura);
-      inspect(imc);
+      final double imc = peso / (altura * altura);
+
+      if (imc < 16) {
+        information = 'magreza grave';
+      } else if (imc > 16 && imc < 17) {
+        information = 'magreza moderada';
+      } else if (imc > 17 && imc < 18.5) {
+        information = 'magreza leve';
+      } else if (imc > 18.5 && imc < 25) {
+        information = 'saudavel';
+      } else if (imc > 25 && imc < 30) {
+        information = 'sobrepeso';
+      } else if (imc > 30 && imc < 35) {
+        information = 'obesidade grau I';
+      } else if (imc > 35 && imc < 40) {
+        information = 'obesidade grau II';
+      } else if (imc >= 40.1) {
+        information = 'saudavel';
+      }
+
+      setState(() {});
       ScaffoldMessenger.of(context)
-          .showSnackBar(SnackBar(content: Text('O seu IMC e $imc')));
+          .showSnackBar(SnackBar(content: Text(information)));
+
+      return imc;
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    inspect(imc);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Calculadora IMC'),
@@ -62,9 +86,37 @@ class _HomePageViewState extends State<HomePageView> {
             ),
           ),
           FilledButton(
-              onPressed: () => calculate(double.parse(pesoController.text),
-                  double.parse(alturaController.text), context),
-              child: const Text('Calcular'))
+              onPressed: () {
+                imc = calculate(double.parse(pesoController.text),
+                    double.parse(alturaController.text), context);
+              },
+              child: const Text('Calcular')),
+          Expanded(
+            child: GridView(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3),
+              children: [
+                Column(
+                  children: [
+                    Text('Peso'),
+                    Text(pesoController.text),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text('Altura'),
+                    Text(alturaController.text),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text('IMC'),
+                    imc == null ? Container() : Text(imc!.toStringAsFixed(2)),
+                  ],
+                ),
+              ],
+            ),
+          )
         ],
       )),
     );
