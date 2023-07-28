c939cad0f2a68306f5b03ec5624f32c262a04178
vinihzets <viniciusdaarky@outlook.com>
2023-07-28 15:02:40 -0300

    adicionado objeto para herdar um HiveObject para conseguir salvar meu objeto no hive.

--- lib/models/pessoa_model.dart
+++ lib/models/pessoa_model.dart
@@ -1,7 +1,34 @@
-class PessoaModel {
+import 'package:hive/hive.dart';
+
+@HiveType(typeId: 0)
+class PessoaModel extends HiveObject {
+  @HiveField(0)
   final String peso;
+  @HiveField(1)
   final String altura;
+  @HiveField(2)
   final double imc;
 
   PessoaModel({required this.peso, required this.altura, required this.imc});
+}
+
+class PessoaModelAdapter extends TypeAdapter<PessoaModel> {
+  @override
+  final int typeId = 0;
+
+  @override
+  PessoaModel read(BinaryReader reader) {
+    return PessoaModel(
+      peso: reader.readString(),
+      altura: reader.readString(),
+      imc: reader.readDouble(),
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, PessoaModel obj) {
+    writer.writeString(obj.peso);
+    writer.writeString(obj.altura);
+    writer.writeDouble(obj.imc);
+  }
 }
